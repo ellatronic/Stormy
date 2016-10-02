0a03f25a3c3437da4b96b5d189c5ec495990eabb
ellatronic <ellachiangg@gmail.com>
2016-10-01 23:23:23 -0700

    Added starter files; set up CurrentWeather constants

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Ella on 10/1/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbabily: Double
+    let summary: String
+    let icon: UIImage
+}
