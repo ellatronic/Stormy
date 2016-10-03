96cce7a9bf60df89808798952ee2ede5a88e0b54
ellatronic <ellachiangg@gmail.com>
2016-10-03 12:49:24 -0700

    Created icon enum

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+extension CurrentWeather {
+    var temperatureString: String {
+        return "\(Int(temperature))º"
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var currentTemperatureLabel: UILabel!
@@ -22,11 +28,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let icon = WeatherIcon.PartlyCloudyDay.image
+        let currentWeather = CurrentWeather(temperature: 56.0, humidity: 1.0, precipitationProbabily: 1.0, summary: "Wet and rainy!", icon: icon)
+        display(weather: currentWeather)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func display(weather: CurrentWeather) {
+        currentTemperatureLabel.text = weather.temperatureString
     }
 
 
